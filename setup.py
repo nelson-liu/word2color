fe8ce0a818b5de2fe8f1b2a4b5cb183f0eb0e946
Nelson Liu <nelson.liu.2009@gmail.com>
2016-04-08 23:06:19 -0700

    feature: create setup.py for module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(author='Nelson Liu, Aakash Sethi',
+      author_email='nfliu@uw.edu, aakash@uw.edu',
+      description='Convert descriptions of colors to colors',
+      install_requires=[],
+      license='MIT',
+      long_description='A tool for transforming natural language descriptions of colors to web-standard color names',
+      name='word2color',
+      packages=['word2color'],
+      url='https://github.com/nelson-liu/word2color',
+      version='0.1',
+      zip_safe=False
+)
